2f59d18e04148f0cc45239b217db03ee624f7eed
Janella Senica <janella.senica@gmail.com>
2020-07-03 13:13:27 -0500

    added giphy

--- giphy.js
+++ giphy.js
@@ -1,29 +1,90 @@
 // Variables
 var gifUrl = "http://api.giphy.com/v1/gifs/search?";
 var APIKey = "YX5nJxh4MAa6JTcABrKTaj04LiiMnc4a";
-var searchQuery = "";
+var queryUrl = gifUrl + "q=" + chef + "&api_key=" + APIKey + "&limit=10";
+var chef = "";
+var gordonRamsey = "gordonramsey";
+var inaGarten = "inagarten";
+var guyFieri = "guyfieri";
+var marcusSamuelsson = "marcussamuelsson";
 
-// QueryUrl
-var queryUrl = gifUrl + "q=" + searchQuery + "&api_key=" + APIKey + "&limit=5";
-
-$(document).ready(function () {
-
-    // Ajax function
+// Gordon Ramsey Button
+$("#gordonramsey").click(function () {
+    event.preventDefault();
+    let chef = "gordonramsey";
+    var queryUrl = gifUrl + "q=" + chef + "&api_key=" + APIKey + "&limit=10";
+    console.log(queryUrl);
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
-        $("giphyView").text(JSON.stringify(response));
-        console.log(queryUrl);
+        console.log(response);
+        $("#giphyDiv").empty();
+        for (var i = 0; i < 3; i++) {
+            var item = response.data[i].images.fixed_height.url;
+            $("#giphyDiv").append("<img src='" + item + "'>");
+        }
 
-        // Not sure if separate button will prompt gifs??
-        $("button name").on("click", function () {
-            var getGifs = response.length;
-            for (var i = 0; i < response.length; i++) {
-                var random = Math.floor(Math.random() * getGifs);
-                getGifs = random + getGifs;
-            }
-        })
     });
+});
+// Ina Garten Button
+$("#inagarten").click(function () {
+    event.preventDefault();
+    let chef = "inagarten";
+    var queryUrl = gifUrl + "q=" + chef + "&api_key=" + APIKey + "&limit=10";
+    console.log(queryUrl);
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#giphyDiv").empty();
+        for (var i = 0; i < 3; i++) {
+            var item = response.data[i].images.fixed_height.url;
+            $("#giphyDiv").append("<img src='" + item + "'>");
+        }
 
+    });
+});
+// Guy Fieri Button
+$("#guyfieri").click(function () {
+    event.preventDefault();
+    let chef = "guyfieri";
+    var queryUrl = gifUrl + "q=" + chef + "&api_key=" + APIKey + "&limit=10";
+    console.log(queryUrl);
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#giphyDiv").empty();
+        // Gif1
+        var item = response.data[0].images.fixed_height.url;
+        $("#giphyDiv").append("<img src='" + item + "'>");
+        // Gif2
+        var item = response.data[2].images.fixed_height.url;
+        $("#giphyDiv").append("<img src='" + item + "'>");
+        // Gif3
+        var item = response.data[3].images.fixed_height.url;
+        $("#giphyDiv").append("<img src='" + item + "'>");
+    });
+});
+// Padma Lakshmi Button
+$("#padmalakshmi").click(function () {
+    event.preventDefault();
+    let chef = "padmalakshmi";
+    var queryUrl = gifUrl + "q=" + chef + "&api_key=" + APIKey + "&limit=10";
+    console.log(queryUrl);
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#giphyDiv").empty();
+        for (var i = 0; i < 3; i++) {
+            var item = response.data[i].images.fixed_height.url;
+            $("#giphyDiv").append("<img src='" + item + "'>");
+        }
+
+    });
 });
